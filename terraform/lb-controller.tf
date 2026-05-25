2684ec83c40ef4dcd9da2ffa531818898a14d0af
Ihedioha Ifeyinwa <ihediohaifeyinwa@gmail.com>
2026-05-25 20:56:42 +0100

    added load balancer controller IAM role and policy

---
+++ terraform/lb-controller.tf
@@ -0,0 +1,32 @@
+resource "aws_iam_policy" "aws_load_balancer_controller" {
+  name = "AWSLoadBalancerControllerIAMPolicy"
+
+  policy = file("${path.module}/lb-controller-policy.json")
+}
+
+resource "aws_iam_role" "aws_load_balancer_controller" {
+  name = "AWSLoadBalancerControllerRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = module.eks.oidc_provider_arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller" {
+  role       = aws_iam_role.aws_load_balancer_controller.name
+  policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
+}
