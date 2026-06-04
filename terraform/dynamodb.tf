f48114295548989ab77fdbdaf512d218ec5ffcb8
Ihedioha Ifeyinwa <ihediohaifeyinwa@gmail.com>
2026-06-04 11:25:52 +0100

    feat: added Items DynamoDB table for carts service

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -8,3 +8,14 @@ resource "aws_dynamodb_table" "cart" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table" "items" {
+  name         = "Items"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
